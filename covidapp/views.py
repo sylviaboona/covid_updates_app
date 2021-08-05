0f5b3bf8d443cd3d69004589a54ed2d69a25b51b
Sylvia Boona <sylvieboona@gmail.com>
2021-08-05 11:47:45 +0300

    styling

--- covidapp/views.py
+++ covidapp/views.py
@@ -24,19 +24,19 @@ response = requests.request("GET", url, headers=headers).json()
 
 # Create your views here.
 def covid_stats_view(request):
-    mylist = []
-    noofresults = int(response['results'])
+    country_list = []
+    total_no_of_results = int(response['results'])
 
-    for x in range(0, noofresults):
+    for x in range(0, total_no_of_results):
 
-        mylist.append(response['response'][x]['country'])
+        country_list.append(response['response'][x]['country'])
 
     if request.method == 'POST':
-        selectedcountry = request.POST['selectedcountry']
+        selected_country = request.POST['selectedcountry']
 
         # Getting  cases for each country
-        for x in range(0, noofresults):
-            if selectedcountry == response['response'][x]['country']:
+        for x in range(0, total_no_of_results):
+            if selected_country == response['response'][x]['country']:
                 new = response['response'][x]['cases']['new']
                 active = response['response'][x]['cases']['active']
                 critical = response['response'][x]['cases']['critical']
@@ -44,11 +44,11 @@ def covid_stats_view(request):
                 total = response['response'][x]['cases']['total']
                 deaths = int(total) - int(active)-int(recovered)
 
-        context = {'selectedcountry': selectedcountry, 'mylist': mylist, 'new': new, 'active': active,
+        context = {'selectedcountry': selected_country, 'country_list': country_list, 'new': new, 'active': active,
                    'critical': critical, 'recovered': recovered, 'deaths': deaths, 'total': total}
         return render(request, 'covidapp/covid_stats.html', context)
 
-    noofresults = int(response['results'])
-    mylist.append(response['response'][x]['country'])
-    context = {'mylist': mylist}
+    total_no_of_results = int(response['results'])
+    country_list.append(response['response'][x]['country'])
+    context = {'country_list': country_list}
     return render(request, 'covidapp/covid_stats.html', context)
